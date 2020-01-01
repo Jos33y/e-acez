81f094ca6eef0bd9e0db1bf2ebfea78f1a289563
Jos33y <josephlagbalu@gmail.com>
2020-01-01 21:30:31 +0000

    deleted files and error in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,57 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React , { useState } from "react"
+import Img from "gatsby-image"
+import { graphql } from "gatsby"
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+
+const Header = ({data}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+  
+      <Navbar navbar fixed="top" light expand="sm" id="mainNav">
+     <div className="container">
+        <NavbarBrand href="/">
+        <Img  fixed={data.file.childImageSharp.fixed} />
+        <span className="logo-head">e-acez </span>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/blog">Blog</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/team">Team</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/tags">Tags</NavLink>
+            </NavItem>
+            </Nav>
+        </Collapse>
+        </div>
+      </Navbar>
+    
 )
+
+  }
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -40,3 +62,17 @@ Header.defaultProps = {
 }
 
 export default Header
+
+export const query = graphql`
+query {
+  file(relativePath: { eq: "images/eicon.png" }) {
+    childImageSharp {
+      # Specify the image processing specifications right in the query.
+      # Makes it trivial to update as your page's design changes.
+      fixed(width: 125, height: 125) {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+}
+`
---
+++ src/pages/tags.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TagsPage = () => (
+  <Layout>
+    <SEO title="Tag" />
+    <h1>Post Tags</h1>
+  </Layout>
+)
+
+export default TagsPage
